32862d9b77690c11b4d2020fe90323fd9fb0e104
Felipe Teles <felipersteles@gmail.com>
2023-03-31 20:59:37 -0300

    add: generating the bin and the recyclable

--- ASL/main.py
+++ ASL/main.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 
 def return_zero():
     print('Starting function return_zero()...')
@@ -13,7 +13,7 @@ class Position:
 
 
 class Robo:
-    value = 1
+    value = "R"
     inTrashPosition = False
 
     def __init__(self, name, position):
@@ -27,7 +27,9 @@ class Robo:
         self.position = position
 
     def checkPosition(self, position):
-        if(position==Position(19,19)):
+        print("Checking position", position.row, position.column)
+        if(position.row == 19 and position.column == 19):
+            print("Trash position")
             self.inTrashPosition = True
 
 
@@ -37,7 +39,7 @@ class Mapa:
     def __init__(self, position):
         self.row = position.row
         self.column = position.column
-        self.matrix = [[0 for x in range(self.row)]
+        self.matrix = [["-" for x in range(self.row)]
                        for y in range(self.column)]
 
     def positionValue(self, position):
@@ -49,56 +51,82 @@ class Mapa:
             print(term)
 
     def moveRobot(self, position, robo):
-        self.removeRobot(robo)
-        self.addRobot(position, robo)
-        robo.move(position)
-        robo.checkPosition(position)
-        if(robo.inTrashPosition):
-          print("Chegou no lixo")
+        if(robo.inTrashPosition != True):
+            self.removeRobot(robo)
+            self.addRobot(position, robo)
+            robo.move(position)
+            robo.checkPosition(position)
+            if(robo.inTrashPosition):
+                print("Chegou no lixo")
 
     def simpleMoveRobot(self, robo):
       #começando do [0][0]  
-        
+        os.system("cls")
+        self.printMapa()
         while(robo.position.row < self.row-1):
             self.moveRobot(Position(robo.position.row+1,robo.position.column), robo)
 
-        self.moveRobot(Position(robo.position.row, robo.position.column+1), robo)
-        if(robo.inTrashPosition == True):
-            return "chegou"
+        if(robo.inTrashPosition == False):
+            os.system("cls")
+            self.printMapa()
 
-        while(robo.position.row > 0 ):
+        if(robo.position.column + 1 < self.column):
+            self.moveRobot(Position(robo.position.row, robo.position.column+1), robo)
+
+        if(robo.inTrashPosition == False):
+            os.system("cls")
+            self.printMapa()
+
+        while(robo.position.row > 0 and robo.inTrashPosition == False):
             self.moveRobot(Position(robo.position.row-1,robo.position.column), robo)
         
-        self.moveRobot(Position(robo.position.row, robo.position.column+1), robo)
+        if(robo.inTrashPosition == False):
+            os.system("cls")
+            self.printMapa()
+
+        if(robo.position.column + 1 < self.column):
+            self.moveRobot(Position(robo.position.row, robo.position.column+1), robo)
 
     def addRobot(self, position, robo):
         self.matrix[position.row][position.column] = robo.value
 
     def removeRobot(self, robo):
-        self.matrix[robo.position.row][robo.position.column] = 0
+        self.matrix[robo.position.row][robo.position.column] = "x"
 
-    def addTrash(self, value):
-        self.matrix[19][19] = value
+    def addTrash(self, position):
+        self.matrix[position.row][position.column] = "T"
 
-    def addBin(self, value):
-        self.matrix[0][0] = value
+    def addBin(self, position):
+        self.matrix[position.row][position.column] = "B"
 
+    def addRecyclable(self, position):
+        self.matrix[position.row][position.column] = "L"
+        
+    def positionItems(self):
+        random.seed(0)
+        for i in range(10):
+            self.addBin(Position(random.randint(0, 19), random.randint(0, 19)))
+
+        for i in range(5):
+            self.addRecyclable(Position(random.randint(0, 19), random.randint(0, 19)))
+            
 
 class Mundo:
     def __init__(self):
         self.agente = Robo("robo", Position(0, 0))
         self.mapa = Mapa(Position(20, 20))
         self.mapa.addRobot(Position(0, 0), self.agente)
-        self.mapa.addTrash(3)
+        self.mapa.addTrash(Position(19, 19))
+        self.mapa.positionItems()
 
     def robotSimpleMovement(self):
-      while(self.mapa.simpleMoveRobot(self.agente) != 'chegou'):
-        self.mapa.simpleMoveRobot(self.agente)
         self.mapa.printMapa()
 
         
         #for i in range(100000):
           #self.mapa.addBin(0)
+
+    
     def start(self):
         self.robotSimpleMovement()
 
