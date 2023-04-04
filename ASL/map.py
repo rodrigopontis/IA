e2a2b23bfc6946965e973b66609710d22323ce42
Felipe Teles <felipersteles@gmail.com>
2023-04-04 13:45:00 -0300

    add: robot with objectives

--- ASL/map.py
+++ ASL/map.py
@@ -21,6 +21,12 @@ class CollectableObject:
             return self.position == comp.position
 
         return None
+    
+    def __lt__(self, comp):
+        if comp != None:
+            return self.position < comp.position
+        
+        return None
 
     def __hash__(self):
         return hash((self.value, self.position))
@@ -48,22 +54,30 @@ class Map:
         #Organic 10
         g1 = CollectableObject(Position(1, 6))
         self.garbageItems.append(g1)
+        
         g2 = CollectableObject(Position(2, 18))
         self.garbageItems.append(g2)
+
         g3 = CollectableObject(Position(3, 7))
         self.garbageItems.append(g3)
+
         g4 = CollectableObject(Position(6, 15))
         self.garbageItems.append(g4)
+
         g5 = CollectableObject(Position(7, 2))
         self.garbageItems.append(g5)
         g6 = CollectableObject(Position(9, 9))
         self.garbageItems.append(g6)
+
         g7 = CollectableObject(Position(13, 2))
         self.garbageItems.append(g7)
+
         g8 = CollectableObject(Position(14, 9))
         self.garbageItems.append(g8)
+
         g9 = CollectableObject(Position(16, 12))
         self.garbageItems.append(g9)
+
         g10 = CollectableObject(Position(18, 15))
         self.garbageItems.append(g10)
 
@@ -71,15 +85,19 @@ class Map:
         g11 = CollectableObject(Position(1, 2))
         g11.recyclable()
         self.garbageItems.append(g11)
+
         g12 = CollectableObject(Position(5, 18))
         g12.recyclable()
         self.garbageItems.append(g12)
+
         g13 = CollectableObject(Position(12, 7))
         g13.recyclable()
         self.garbageItems.append(g13)
+
         g14 = CollectableObject(Position(8, 9))
         g14.recyclable()
         self.garbageItems.append(g14)
+        
         g15 = CollectableObject(Position(19, 13))
         g15.recyclable() 
         self.garbageItems.append(g15)
@@ -99,21 +117,23 @@ class Map:
         print("\nMatrix:")
         self.matrix = [["-" for x in range(self.row)] for y in range(self.column)]
 
+        for garbage in self.garbageItems:
+            self.matrix[garbage.position.row][garbage.position.column] = garbage.value
+        
         self.matrix[self.bin.position.row][self.bin.position.column] = self.bin.value
 
         self.matrix[self.robot.position.row][
             self.robot.position.column
         ] = self.robot.value
-        for garbage in self.garbageItems:
-            self.matrix[garbage.position.row][garbage.position.column] = garbage.value
 
         for term in self.matrix:
             print(term)
 
     def collectItem(self, item):
+        print("removendo", item, "do array")
         for garbage in self.garbageItems:
             if item == garbage:
-                self.robot.holdingItem = garbage
+                print("item encontrado")
                 self.garbageItems.remove(garbage)
 
     def isAValidPosition(self, position):
